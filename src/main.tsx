ea738b55f45a8fefb68aa622808236240bf3f5fa
smv-manovihar <sirimallamanovihar@gmail.com>
2026-05-06 22:34:10 +0530

    Update: logo and analytics

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ProgressProvider } from "@/components/ProgressProvider.tsx"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient()
 
@@ -17,6 +19,8 @@ createRoot(document.getElementById("root")!).render(
         <ProgressProvider>
           <App />
           <Toaster position="top-center" />
+          <Analytics />
+          <SpeedInsights />
         </ProgressProvider>
       </ThemeProvider>
     </QueryClientProvider>
